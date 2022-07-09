85e1d8345542386905d539babfb930bb8e4ca99d
andrescab <andcab1293@gmail.com>
2022-07-09 09:57:07 -0700

    ch13.4

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,19 +1,47 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790],
-  [30.2672, -97.7431],
-  [43.6777, -79.6248],
-  [40.7128, -74.0060]
-];
-// Create a polyline using the line coordinates and make the line blue.
-L.polyline(line, {
-  color: "blue"
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup("<h2>" + feature.properties.faa + "</h2>" + "<hr>" + "<h2>" + feature.properties.name + "</h2>")
+   }
 }).addTo(map);
+
+// Coordinates for each point to be used in the polyline.
+//let line = [
+//  [33.9416, -118.4085],
+//  [37.6213, -122.3790],
+//  [30.2672, -97.7431],
+//  [43.6777, -79.6248],
+//  [40.7128, -74.0060]
+//];
+// Create a polyline using the line coordinates and make the line blue.
+//L.polyline(line, {
+//  color: "blue"
+//}).addTo(map);
 // Get data from cities.js
 let cityData = cities;
 // Loop through the cities array and create one marker for each city.
@@ -28,7 +56,7 @@ let cityData = cities;
   //.addTo(map);
 //})
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
