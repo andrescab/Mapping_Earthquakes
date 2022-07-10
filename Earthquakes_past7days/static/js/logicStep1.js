2ec33bea4cbe37fda1db6c5259785eded1a8a565
andrescab <andcab1293@gmail.com>
2022-07-10 08:34:21 -0700

    13.6.1

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -20,9 +20,9 @@ let baseMaps = {
 };
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  "center": [43.7, -79.3],
-  "zoom": 11,
-  "layers": [satelliteStreets]
+  "center": [39.5, -98.5],
+  "zoom": 3,
+  "layers": [streets]
 })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
@@ -31,12 +31,18 @@ L.control.layers(baseMaps).addTo(map);
 //streets.addTo(map);
 /// Accessing the Toronto neighborhoods GeoJSON URL.
 let torontoHoods = "https://raw.githubusercontent.com/andrescab/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addto(map);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
 });
+
+// Grabbing our GeoJSON data.
+//d3.json(torontoHoods).then(function(data) {
+  //console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+//L.geoJSON(data).addto(map);
+//});
 // Creating a GeoJSON layer with the retrieved data.
 //L.geoJSON(data, {
 //  style: myStyle,
